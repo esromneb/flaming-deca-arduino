9e076708b1cb4519a9c3372183931f1a48818d67
Benjamin Morse <bjmorse@gmail.com>
2015-04-27 00:46:13 -0700

    happy with random series

--- LightNecklace/LightNecklace.ino
+++ LightNecklace/LightNecklace.ino
@@ -12,6 +12,11 @@ const int redPin = 3;
 const int greenPin = 5;
 const int bluePin = 6;
 const int pins[3] = {redPin, greenPin, bluePin};
+unsigned master = 1;
+unsigned long eventStart;
+unsigned long eventEnd;
+unsigned long nextEventStart;
+unsigned long nextEventEnd;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -22,6 +27,50 @@ void setup() {
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT); 
+  eventStart = 0;
+  eventEnd = 4000;
+}
+
+void greenRamp(unsigned long tstart, unsigned long tend)
+{
+  unsigned long now = millis();
+  while(now<tend)
+  {
+    analogWrite(greenPin, map(now, tstart, tend, 0, 255));
+    now = millis();
+  }
+}
+
+void strobeBlue(unsigned long tstart, unsigned long tend)
+{
+  char buf[64];
+
+  unsigned long now = millis();
+  unsigned sections = random()%20+5;
+  unsigned long length = (tend-tstart)/sections;
+//  Serial.print("length");
+//  Serial.println(length);
+//  sprintf(buf, "len %ld tstart %ld tend %ld\r\n", length, tstart, tend);
+//  Serial.print(buf);
+  while(now<tend)
+  {
+    if( now%(length*2) < length )
+    {
+      analogWrite(bluePin, 0);
+    }
+    else
+    {
+      analogWrite(bluePin, 255);
+    }
+//    Serial.println(now%length);
+    now = millis();
+  }
+}
+
+void duration(unsigned long *tstart, unsigned long *tend, unsigned long bump)
+{
+  *tstart = *tend;
+  *tend = *tend + bump;
 }
 
 // the loop routine runs over and over again forever:
@@ -31,9 +80,28 @@ void loop() {
   unsigned long now = millis();
   
   unsigned long ramp_start = 0;
-  unsigned long ramp_end = 5000;
+  unsigned long ramp_end = 4000;
   
-  analogWrite(greenPin, map(now - ramp_start, ramp_start, ramp_end, 0, 255));
+  while(1)
+  {
+  greenRamp(eventStart, eventEnd);
+  analogWrite(greenPin,0);
+  delay(1);
+  duration(&eventStart, &eventEnd, 100+random()%7777);
+
+    
+  strobeBlue(eventStart, eventEnd);
+  analogWrite(bluePin,0);
+  delay(1);
+  duration(&eventStart, &eventEnd, 100+random()%7777);
+  }
+  
+
+  
+  analogWrite(greenPin,0);
+    while(1){}
+  
+
   
 //  int thisPin = pins[r%3];
 //  analogWrite(thisPin, max(val%256, 200));
