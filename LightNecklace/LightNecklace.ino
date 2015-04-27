205de701c86f941d7a22de32eaf5398896cd791c
Benjamin Morse <bjmorse@gmail.com>
2015-04-26 23:12:22 -0700

    green ramp

--- LightNecklace/LightNecklace.ino
+++ LightNecklace/LightNecklace.ino
@@ -7,18 +7,45 @@
  
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 13;
+
+const int redPin = 3;
+const int greenPin = 5;
+const int bluePin = 6;
+const int pins[3] = {redPin, greenPin, bluePin};
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+
+  Serial.begin(9600);
+  randomSeed(42);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT); 
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+void loop() {  
+  unsigned r = random();
+  unsigned val = random();
+  unsigned long now = millis();
+  
+  unsigned long ramp_start = 0;
+  unsigned long ramp_end = 5000;
+  
+  analogWrite(greenPin, map(now - ramp_start, ramp_start, ramp_end, 0, 255));
+  
+//  int thisPin = pins[r%3];
+//  analogWrite(thisPin, max(val%256, 200));
+//  
+//  delay(2000);
+  
+//  analogWrite(greenPin,255);
+//    analogWrite(bluePin,255);
+//        analogWrite(redPin,150);
+
+  
+  
+//  Serial.print(F("%d pick"));
+//  Serial.println(r);
 }
